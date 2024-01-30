30d310fea4454c280025e63da3a2caf9144d0273
Chaps0218 <mathiguevara@gmail.com>
2024-01-29 22:06:59 -0500

    Funcionalidad Cámara

--- app/src/main/java/com/example/giroscopio/Camera.java
+++ app/src/main/java/com/example/giroscopio/Camera.java
@@ -1,14 +1,164 @@
 package com.example.giroscopio;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.core.content.FileProvider;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class Camera extends AppCompatActivity {
-
+    public static final int GALLERY_REQUEST_CODE = 105;
+    public static final int CAM_PERM_CODE = 101;
+    public static final int CAM_REQUEST_CODE = 102;
+    ImageView imageView;
+    Button camera, galeria;
+    String currentPhotoPath;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_camera);
+
+        imageView = findViewById(R.id.ivFoto);
+        camera = findViewById(R.id.btnCamara);
+        galeria = findViewById(R.id.btnGaleria);
+
+        camera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                askCameraPermission();
+            }
+        });
+        galeria.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent galeria = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(galeria, GALLERY_REQUEST_CODE);
+            }
+        });
     }
+
+    private void askCameraPermission() {
+        if(ContextCompat.checkSelfPermission(this, android.Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this, new String[] {android.Manifest.permission.CAMERA}, CAM_PERM_CODE);
+        }else{
+            dispatchTakePictureIntent();
+        }
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if ((requestCode == CAM_PERM_CODE) && (grantResults.length > 0) && (grantResults[0] == PackageManager.PERMISSION_GRANTED)){
+            dispatchTakePictureIntent();
+        }else{
+            Toast.makeText(this, "Se requieren permisos de camara", Toast.LENGTH_SHORT).show();
+        }
+    }
+    private void openCamera() {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(intent, CAM_REQUEST_CODE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == CAM_REQUEST_CODE){
+            if (resultCode == Activity.RESULT_OK){
+                File f = new File(currentPhotoPath);
+                Uri contentUri = Uri.fromFile(f);
+                imageView.setImageURI(contentUri);
+                Log.d("tag", "Absolute Url of Image is " + Uri.fromFile(f));
+                Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+                mediaScanIntent.setData(contentUri);
+                this.sendBroadcast(mediaScanIntent);
+            }
+        }
+
+        if (requestCode == GALLERY_REQUEST_CODE){
+            if (resultCode == Activity.RESULT_OK){
+                Uri contentUri = data.getData();
+                String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+                String imageFileName = "Img_" + timeStamp + "_";
+                String subfolderName = "GiroscopioApp";
+                File storageDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+                File subfolder = new File(storageDir, subfolderName);
+                if (!subfolder.exists()){
+                    subfolder.mkdirs();
+                }
+                File image = null;
+                try {
+                    image = File.createTempFile(
+                            imageFileName,  /* prefix */
+                            ".jpg",         /* suffix */
+                            subfolder      /* directory */
+                    );
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                currentPhotoPath = image.getAbsolutePath();
+                imageView.setImageURI(contentUri);
+            }
+        }
+    }
+
+    private File createImageFile() throws IOException {
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "Img_" + timeStamp + "_";
+        String subfolderName = "GiroscopioApp";
+        File storageDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+        File subfolder = new File(storageDir, subfolderName);
+        if (!subfolder.exists()){
+            subfolder.mkdirs();
+        }
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                subfolder      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        currentPhotoPath = image.getAbsolutePath();
+        return image;
+    }
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+
+            }
+            if (photoFile != null) {
+                Uri photoURI = FileProvider.getUriForFile(this,
+                        "com.example.giroscopio.fileprovider",
+                        photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                //noinspection deprecation
+                startActivityForResult(takePictureIntent, 102);
+            }
+        }
+    }
+
+
 }
